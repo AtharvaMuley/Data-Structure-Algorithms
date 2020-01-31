0f35b3cbc4566b3257b3a1a3c6e8d0536ae1c2e1
Atharva <atharvamuley1@gmail.com>
2020-01-30 22:01:40 -0500

    added more operation to Linked list

--- DS/Linked_List.py
+++ DS/Linked_List.py
@@ -2,22 +2,48 @@ class ListNode:
     def __init__(self, value):
         self.val = value
         self.next = None
-    
+
 class List(ListNode):
     def __init__(self):
         self.head = None
         self.tail = None
         self.node_count = 0
 
-    def addNode(self, value):
-        if self.head == None:
-            self.head = self.tail = ListNode(value)
-        else:
-            temp = ListNode(value)
-            self.tail.next = temp
-            self.tail = temp
-        self.node_count += 1
+    def insertNode(self, values):
+        if type(values) != list:
+            values = [values]
+
+        for each in values:
+            if self.head == None:
+                self.head = self.tail = ListNode(each)
+            else:
+                temp = ListNode(each)
+                self.tail.next = temp
+                self.tail = temp
+            self.node_count += 1
     
+    #Removes the first occurence of the Element
+    def removeElement(self, value):
+        curr = self.head;prev=None;next=None
+        while curr != None:
+            next = curr.next
+            if curr.val == value:
+                if prev:
+                    prev.next = next
+                    del curr
+                else:
+                    self.head = next
+                self.node_count -= 1
+                return
+            prev = curr
+            curr = curr.next
+        
+        print("Element Not Present in the list")
+
+    #Returns the number of nodes present in the list
+    def getNodeCount(self):
+        return self.node_count
+
     def printList(self):
         temp = self.head
         while temp != None:
@@ -28,7 +54,9 @@ class List(ListNode):
 
 if __name__ == "__main__":
     custom_list = List()
-    custom_list.addNode(4)
-    custom_list.addNode(1)
-    custom_list.addNode(2)
+    custom_list.insertNode([4,1,2,3,4])
+    custom_list.insertNode(6)
+    custom_list.insertNode(7)
+    # custom_list.removeElement(1)
     custom_list.printList()
+    print(custom_list.getNodeCount())
